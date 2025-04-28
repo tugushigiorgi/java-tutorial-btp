99b4511f7345664aadc987875f67131d10a63123
Giorgi Tughushi <giorgi.tughushi@leverx.com>
2025-04-28 16:29:02 +0400

    add logging service for sap

--- src/main/java/com/example/java_tutorial/MainController.java
+++ src/main/java/com/example/java_tutorial/MainController.java
@@ -1,5 +1,6 @@
 package com.example.java_tutorial;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -11,14 +12,19 @@ import com.sap.cloud.security.xsuaa.token.Token;
 
 @RestController
 @RequestMapping(path = "")
-
+@Slf4j
 public class MainController {
 
   @GetMapping(path = "")
   public ResponseEntity<String> readAll(@AuthenticationPrincipal Token token) {
     if (!token.getAuthorities().contains(new SimpleGrantedAuthority("Display"))) {
+      log.error("This operation requires \"Display\" scope");
+
+
+
       throw new NotAuthorizedException("This operation requires \"Display\" scope");
     }
+    log.info("hello world   ");
 
     return new ResponseEntity<String>("Hello World!", HttpStatus.OK);
   }

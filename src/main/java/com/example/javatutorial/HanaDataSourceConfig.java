baa2610246b7beb1730198258f670986254b7e12
Giorgi <giorgi.tughushi@leverx.com>
2025-05-15 12:39:41 +0400

    trying to fix liquibase

--- src/main/java/com/example/javatutorial/HanaDataSourceConfig.java
+++ src/main/java/com/example/javatutorial/HanaDataSourceConfig.java
@@ -1,8 +1,10 @@
 package com.example.javatutorial;
-import com.fasterxml.jackson.databind.JsonNode;
+
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zaxxer.hikari.HikariDataSource;
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,24 +15,28 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class HanaDataSourceConfig {
 
+
   @Bean
   @Primary
-  public DataSource hanaDataSource() throws IOException {
-    String vcapServices = System.getenv("VCAP_SERVICES");
+  public DataSource hanaDataSource() throws IOException, SQLException {
+    var vcapServices = System.getenv("VCAP_SERVICES");
     if (vcapServices == null) {
       throw new IllegalStateException("VCAP_SERVICES not found.");
     }
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode root = mapper.readTree(vcapServices);
-    JsonNode hanaNode = root.path("hana").get(0).path("credentials");
-    String url = hanaNode.get("url").asText();
-    String username = hanaNode.get("user").asText();
-    String password = hanaNode.get("password").asText();
+    var mapper = new ObjectMapper();
+    var root = mapper.readTree(vcapServices);
+    var hanaNode = root.path("hana").get(0).path("credentials");
+    var url = hanaNode.get("url").asText();
+    var username = hanaNode.get("hdi_user").asText();
+    var password = hanaNode.get("hdi_password").asText();
     HikariDataSource dataSource = new HikariDataSource();
     dataSource.setJdbcUrl(url);
     dataSource.setUsername(username);
     dataSource.setPassword(password);
     dataSource.setDriverClassName("com.sap.db.jdbc.Driver");
+
+
     return dataSource;
   }
+
 }
--- src/main/java/com/example/javatutorial/HelloWorldApplication.java
+++ src/main/java/com/example/javatutorial/HelloWorldApplication.java
@@ -1,13 +1,21 @@
 package com.example.javatutorial;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class HelloWorldApplication {
+  static DataSource dataSource;
 
   public static void main(String[] args) {
     SpringApplication.run(HelloWorldApplication.class, args);
-    System.out.println("Hello World!");
+
   }
 }
